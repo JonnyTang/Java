bd782ebde51a7e465e066c7bd07feca6132103c6
jonnytang <jonnytang6@gmail.com>
2022-06-18 23:41:29 +0800

    jicheng

--- learningNotes/Inherit.java
+++ learningNotes/Inherit.java
@@ -13,7 +13,7 @@ class Bird implements Flyer {
 
 // 2）对象转换
 // 父类
-class Parent {
+class Parent extends Hero {
 	public String name;
 
 	public void say() {
@@ -37,13 +37,18 @@ class Parent {
 	public void finalize() {
 		System.out.println("这个父类正在被回收");
 	}
-	
+
 	public boolean equals(Object o) {
 		if (o instanceof Parent) {
-			Parent p = (Parent)o;
+			Parent p = (Parent) o;
 			return this.name == p.name;
 		}
 		return false;
+	}
+
+	@Override
+	public void attack() {
+		System.out.println("hero attack enemy with power!");
 	}
 }
 
@@ -80,7 +85,26 @@ class Child1 extends Parent {
 	}
 }
 
+abstract class Hero {
+	String name;
+	int power;
+
+	// 抽象方法attack，子类必要要实现此方法
+	public abstract void attack();
+}
+
 public class Inherit {
+	// 静态内部类
+	static class InnerStatic {
+		int size = 100;
+		
+		public void testInnerStatic() {
+			if (size == 100) {
+				System.out.println("this is a static innner class , the size is " + size);
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("接口和继承！！！");
 
@@ -161,8 +185,67 @@ public class Inherit {
 		// equals()用于判断两个对象的内容是否相同
 		System.out.println(new Parent("jonny").equals(new Parent("xiaodan")));
 		System.out.println(new Parent("jonny").equals(new Parent("jonny")));
-		
+
 		// == 用于判断两个引用，是否指向了同一个对象
 		System.out.println(new Parent("xiaoduo") == new Parent("xiaoduo"));
+
+		// final修饰类、方法、基本类型变量、引用
+		// 当final修饰类时，表示类不能被继承
+		// final修饰方法时，表示该方法不能被重写
+		// final修饰基本类型变量，表示该变量只有一次赋值机会，常量
+		// final修饰引用，表示该引用只有1次指向对象的机会
+
+		// 8、抽象类
+		// 当一个类中有抽象方法时(abstract修改的方法)，该类必须被声明为抽象类; 抽象类不能被实例化
+		/*
+		 * 抽象类与接口的区别： 区别1： 子类只能继承一个抽象类，不能继承多个 子类可以实现多个接口 区别2： 抽象类可以定义
+		 * public,protected,package,private 静态和非静态属性 final和非final属性 但是接口中声明的属性，只能是
+		 * public 静态 final的 即便没有显式的声明
+		 * 
+		 * 注: 抽象类和接口都可以有实体方法。 接口中的实体方法，叫做默认方法
+		 */
+
+		// 9、内部类
+		/*
+		 * 内部类分为四种： 非静态内部类 静态内部类 匿名类 本地类
+		 */
+		// 非静态内部类
+		class Inner{
+			int size;
+			String name;
+			
+			public Inner() {
+				System.out.println("I'm a Inner class.");
+				System.out.println("my size is " + this.size);
+				System.out.println("my name is " + this.name);
+			}
+		}
+		new Inner();
+		
+		// 静态内部类
+		Inherit.InnerStatic stac = new Inherit.InnerStatic();
+		stac.testInnerStatic();
+		
+		// 匿名类：声明一个类的同时实例化它，使代码更加简洁精练
+		// 通常情况下，要使用一个接口或抽象类，都必须创建一个子类，有时候，为了快速使用，直接实例化一个抽象类，并“当场”实现其抽象方法
+		// 既然实现了抽象方法，那么就是一个新的类，只是这个类，没有命名。
+		Hero h = new Hero() {
+			// 当场实现attack方法
+			public void attack() {
+				System.out.println("匿名类实现！");
+			}
+		};
+		h.attack();
+		
+		// 本地类：可以理解为有名字的匿名类
+		// 内部类与匿名类不一样的，内部类必须声明在成员的位置，即与属性和方法平等的位置。
+		// 本地类与匿名类一样，直接声明在代码块里面，可以是主方法，for循环里等。
+		class SomeHero extends Hero {
+			public void attack() {
+				System.out.println("内部匿名类.");
+			}
+		}
+		SomeHero sh = new SomeHero();
+		sh.attack();
 	}
 }
